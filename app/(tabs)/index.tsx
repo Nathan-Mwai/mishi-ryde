7bf071c94c5e232ce8002d6962380b18a0ff3896
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-01-07 13:36:17 +0300

    I added some color as well as set that each time I start my application I clear cache and start the application

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-white'>
-      <Text className='font-bold my-10 mx-15'>
+      <Text className='font-bold my-10 mx-15 text-red-500'>
         My trial was successful
       </Text>
       <Text> Hi from the phone?</Text>
