c359de330a1703d13180c8d4d6e696c1f9194577
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-01-09 08:49:12 +0300

    Started doing expo router method to allow seamless use of components between different screens

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const home = () => {
+  return (
+    <SafeAreaView>
+      <Text>home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default home
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+// Was initially called history
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>history</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Rides
