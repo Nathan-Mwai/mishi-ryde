e437afdb5db44f38683ca5e9c9f39fc3d069811e
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-01-09 09:06:46 +0300

    Created the Swipe option and it is working so well moving on to creating a custom component

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,7 +1,7 @@
 import { onboarding } from '@/constants'
 import { router } from 'expo-router'
 import { useRef, useState } from 'react'
-import {  Text, TouchableOpacity, View } from 'react-native'
+import {  Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Swiper from 'react-native-swiper'
 
@@ -29,8 +29,23 @@ const Onboarding = () => {
       >
         {
           onboarding.map((item) => (
-            <View>
-              <Text>{item.title}</Text>
+            <View 
+              className='flex items-center justify-center p-5'
+              key={item.id}
+            >
+              <Image 
+                source={item.image}
+                className='w-full h-[300px]'
+                resizeMode='contain'
+              />
+              <View className='flex flex-row items-center justify-center w-full mt-10'>
+                <Text className='text-black text-3xl font-bold mx-10 text-center'>
+                  {item.title}
+                </Text>
+              </View>
+              <Text className='text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>
+                {item.description}
+              </Text>
             </View>
           ))
         }
