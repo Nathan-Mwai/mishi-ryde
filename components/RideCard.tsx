04dcb136feb71499870e0a7a539b65479ed01786
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-01-13 01:13:36 +0300

    The map is now being rendered well andI can see it well

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Ride } from '@/types/type'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Ride } from "@/types/type";
 
-const RideCard = ({ride: {
+const RideCard = ({
+  ride: {
     destination_longitude,
     destination_latitude,
     origin_address,
@@ -10,13 +11,26 @@ const RideCard = ({ride: {
     created_at,
     ride_time,
     driver,
-    payment_status
-}}: {ride:Ride}) => {
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => {
   return (
-    <View>
-      <Text className='text-3xl'>{driver.first_name}</Text>
+    <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+      <View className="flex flex-row items-center justify-between p-3">
+        <View className="flex flex-row items-center justify-between">
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+            className="w-[80px] h-[90px] rounded-lg "
+          />
+        </View>
+      </View>
+      <Text className="text-3xl">{driver.first_name}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default RideCard
+export default RideCard;
