a5d2295b7e471c64c12e23870f06842a6df20d9a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-01-13 01:00:24 +0300

    Learnt how to destructure using typescript

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -2,10 +2,19 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Ride } from '@/types/type'
 
-const RideCard = ({ride}: {ride:Ride}) => {
+const RideCard = ({ride: {
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status
+}}: {ride:Ride}) => {
   return (
     <View>
-      <Text className='text-3xl'>{ride.driver.first_name}</Text>
+      <Text className='text-3xl'>{driver.first_name}</Text>
     </View>
   )
 }
